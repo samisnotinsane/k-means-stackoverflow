3e467136cb80cd7ef07afca2b25282d7406bb753
starfighter-ix <ec14370@qmul.ac.uk>
2017-11-27 15:16:55 +0000

    Initial commit with python driver.

---
+++ k_means/driver.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+"""
+" Opens a file and returns an object reference to it.
+"""
+def getfile(file_path):
+    data = open(file_path, "r")
+    return data
+
+"""
+" Given a file, prints all attributes for specified row.
+"""
+def printallrowattrib(data, attrib_name):
+    soup = BeautifulSoup(data, 'html.parser')
+    all_rows = soup.find_all('row')
+    attrib_rows = soup.row[attrib_name]
+    for attrib_row in attrib_rows:
+        print(attrib_row)    
+
+# Treat as 'main' method.
+if __name__ == '__main__':
+    f_path = '/Users/sameenislam/Documents/Big_Data/cw2/sample_data/user_sample.xml'
+    data = getfile(f_path)
+    printallrowattrib(data, 'reputation')
+    data.close()
