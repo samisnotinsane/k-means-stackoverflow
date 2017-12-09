aef1e12ec7374dee87f0b112055bd0f18306ba45
starfighter-ix <ec14370@qmul.ac.uk>
2017-12-09 12:20:07 +0000

    Adding parser which joins user and post data and filters appropriate fields using join.

--- k_means/rddparser.py
+++ k_means/rddparser.py
@@ -1,35 +1,44 @@
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext, SparkConf, SQLContext
 from time import time
 from pprint import pprint
+from numpy import array
 
-if __name__ == '__main__':
-    def transformXmlToMap(line):
-        line_nowhitespace = line.strip() # Remove whitespaces.
-        line_noxml = line_nowhitespace[5:-3] # Discard XML tag.
-        raw_elements = line_noxml.split("\"") # Use " as delimiter.
-        i = 0
-        d = {}
-        while i < (len(raw_elements) - 1):
-            key = raw_elements[i].strip()[:-1] # Gets rid of whitespace &'=' sign.
-            value = raw_elements[i+1]
-            # Uncomment print lines for debugging.
-            # print('key:' + key)
-            # print('value:' + value)
-            d[key] = value
-            i += 2
-        return d
-    
-    # Commonly used paths:
-    # /data/stackOverflow2017/Users.xml
-    # so2017/users_sample.xml
-    users_path = "/Users/sameenislam/Documents/Big_Data/cw2/sample_data/user_sample.xml"
-    conf = SparkConf().setAppName("rddparser")
-    sc = SparkContext(conf=conf)
-    raw_data = sc.textFile(users_path)
-    print('Total count: ' + str(raw_data.count()))
-    xml_data = raw_data.map(transformXmlToMap).cache()
+conf = SparkConf().setAppName("rddparser")
+sc = SparkContext(conf=conf)
 
-    total_count = raw_data.count()
-    print('Total count: ' + str(total_count))
-    grouped_data = xml_data.groupByKey()
-    
+users_path = "/data/stackOverflow2017/Users.xml"
+#create RDD from Users.xml
+usersData = sc.textFile(users_path)
+#split line by semicolon
+noSemData = usersData.map(lambda line: line.split("\""))
+#discard file header and keep Id and Reputation data 
+idRepData = noSemData.filter(lambda line: len(line)>5).map(lambda line: (line[0],line[1],line[2],line[3]))
+#filter out lines without "id" field
+idData = idRepData.filter(lambda line : "id" in line[0].lower())
+#filter out lines without "reputation" field
+reputationData = idData.filter(lambda line : "reputation" in line[2].lower())
+#keep Id and Reputation values
+idRep = reputationData.map(lambda line: (line[1],line[3]))
+
+posts_path = "/data/stackOverflow2017/Posts.xml"
+#create RDD from Posts.xml
+postsData = sc.textFile(posts_path)
+#split line by "owneruserid"
+splitPosts = postsData.map(lambda line: line.lower().split("owneruserid="))
+#discard file header and keep OwnerUserId data
+ownerIdData = splitPosts.filter(lambda line: len(line)>1).map(lambda line: (line[1]))
+#split line by semicolon
+ownerIdValues = ownerIdData.map(lambda line: line.split("\""))
+#keep OwnerUserId values 
+ownerId = ownerIdValues.filter(lambda line: len(line)>1).map(lambda line: (line[1]))
+
+#count number of posts for each user
+count = ownerId.map(lambda x: (x[0], 1)).reduceByKey(lambda x, y: 1 + y)
+ 
+#join Id/Reputation RDD with Id/number of posts RDD
+joinRdd = idRep.map(lambda x:(x[0],x[1])).join(count.map(lambda x:(x[0],x[1])))
+
+#create RDD with values needed for clustering
+repNoPosts = joinRdd.map(lambda (k,v): v)
+
+print(joinRdd.take(100))
